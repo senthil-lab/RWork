581224cc25fe4652fd714a3cb3f10f804d5a227a
senthil-lab <Senthilrajah.Narayanan.nandhakumar@ascension.org>
2020-01-21 22:35:22 -0500

    Added scripts for downloading, wrangling, analysing the murders data

---
+++ projects/murders/wrangleData.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+murders <- read.csv("projects/murders/data/murders.csv")
+murders <- murders %>% mutate(region = factor(region), rate = total/population * 10^5)
+save(murders, file = "projects/murders/rda/murders.rda")
+
+murders
